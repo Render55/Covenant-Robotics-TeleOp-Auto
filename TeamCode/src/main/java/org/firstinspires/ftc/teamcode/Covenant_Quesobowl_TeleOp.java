947c3830e87de0272e98a53eacf1dc548a1a62ef
Pixelated-Robotics <teampixelated20739@gmail.com>
2025-04-24 12:15:56 -0500

    Tuning up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Covenant_Quesobowl_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Covenant_Quesobowl_TeleOp.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
@@ -23,6 +24,15 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
         DcMotor leftHang = hardwareMap.dcMotor.get("lift1");
         DcMotor rightHang = hardwareMap.dcMotor.get("lift2");
 
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftHang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightHang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         Servo leftExtension = hardwareMap.get(Servo.class, "leftExtension");
         Servo rightExtension = hardwareMap.get(Servo.class, "rightExtension");
         Servo extensionClaw = hardwareMap.get(Servo.class, "extensionClaw");
@@ -34,15 +44,17 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
         Servo verticalClawAngle = hardwareMap.get(Servo.class, "verticalClawAngle");
         Servo leftArm = hardwareMap.get(Servo.class, "leftArm");
         Servo rightArm = hardwareMap.get(Servo.class, "rightArm");
+        Servo flag = hardwareMap.get(Servo.class, "flag");
 
         String currentGrabPosition = "slidesIn";
-        String grabSpecimen = "False";
-        String transferPrep = "True";
+        String wallGrab = "True";
         String verticalGrab = "False";
         String wallCycling = "False";
-        double pos = extensionPivotClaw.getPosition();
-        boolean leftBumperPrev = false;
-        boolean rightBumperPrev = false;
+        boolean lastLeftBumper = false;
+        boolean lastRightBumper = false;
+        ElapsedTime timer = new ElapsedTime();
+        int step = 0;
+        boolean isRunningSequence = false;
 
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -62,8 +74,9 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
         leftArm.setPosition(0.25); //Transfer Prep Position
         rightArm.setPosition(0.8); //Transfer Prep Position
         verticalClawAngle.setPosition(0.6); //Faced Down
-        verticalPivotClaw.setPosition(0.92); //Straight
+        verticalPivotClaw.setPosition(0.25); //Straight
         verticalClaw.setPosition(0.65); //Open
+        flag.setPosition(0.6); //Flyin
 
         waitForStart();
 
@@ -71,7 +84,7 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
 
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x; //Todo - x1.1 - Counteract imperfect strafing
+            double x = gamepad1.left_stick_x * 0.9;
             double rx = gamepad1.right_stick_x;
 
             // Denominator is the largest motor power (absolute value) or 1
@@ -88,17 +101,18 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
 
-            leftSlide.setPower(gamepad2.left_stick_y);
-            rightSlide.setPower(gamepad2.left_stick_y);
+            if (!isRunningSequence) {
+                leftSlide.setPower(gamepad2.left_stick_y);
+                rightSlide.setPower(gamepad2.left_stick_y);
+            }
 
             if (gamepad1.dpad_up) {
                 //Transfer Prep
-                transferPrep = "True";
                 wallCycling = "False";
                 leftArm.setPosition(0.25);
                 rightArm.setPosition(0.8);
                 verticalClaw.setPosition(0.65);
-                verticalPivotClaw.setPosition(0.9);
+                verticalPivotClaw.setPosition(0.25);
                 extensionPivotClaw.setPosition(0.73);
                 extensionLeftPivot.setPosition(0);
                 extensionRightPivot.setPosition(0.95);
@@ -112,12 +126,11 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
             }
             else if (gamepad1.dpad_down) {
                 //Transfer Prep
-                transferPrep = "True";
                 wallCycling = "False";
                 leftArm.setPosition(0.25);
                 rightArm.setPosition(0.8);
                 verticalClaw.setPosition(0.65);
-                verticalPivotClaw.setPosition(0.9);
+                verticalPivotClaw.setPosition(0.25);
                 extensionPivotClaw.setPosition(0.73);
                 extensionLeftPivot.setPosition(0);
                 extensionRightPivot.setPosition(0.95);
@@ -142,65 +155,87 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
                             idle();
                         }
                         //Go to Place on Bar
-                        leftArm.setPosition(0.1);
-                        rightArm.setPosition(0.95);
-                        verticalClawAngle.setPosition(0.1);
-                        transferPrep = "False";
+                        leftArm.setPosition(0.2);
+                        rightArm.setPosition(0.85);
+                        verticalClawAngle.setPosition(0.2);
                     }
                 //Vertical Claw Release
                 } else if (gamepad1.a && verticalGrab.equals("True"))
                     if (wallCycling.equals("True")) {
                     verticalClaw.setPosition(0.65);
+                    wallCycling = "False";
                     verticalGrab = "False";
                     //Go to grab from Wall
-                    leftArm.setPosition(0.6);
-                    rightArm.setPosition(0.45);
-                    verticalClawAngle.setPosition(0.2);
-                    transferPrep = "False";
+                    leftArm.setPosition(0.55);
+                    rightArm.setPosition(0.5);
+                    verticalClawAngle.setPosition(0.25);
+                    long startTime = System.currentTimeMillis();
+                    while (System.currentTimeMillis() - startTime < 250 && opModeIsActive()) {
+                        // Could check gamepad or update telemetry here
+                        idle();
+                    }
+                    wallCycling = "True";
                     } else { //Wall Cycling = False
                     verticalClaw.setPosition(0.65);
-                    verticalGrab = "False";
+                }
+
+                if (gamepad2.y && !isRunningSequence) {
+                    isRunningSequence = true;
+                    step = 0;
+                    timer.reset();
                 }
 
                 //Transfer and placing position for bucket
-                if (gamepad1.y && wallCycling.equals("False")) {
-                        verticalClaw.setPosition(0.65);
-                        verticalPivotClaw.setPosition(0.9);
-                        extensionPivotClaw.setPosition(0.73);
-                        extensionLeftPivot.setPosition(0);
-                        extensionRightPivot.setPosition(0.95);
-                        verticalClawAngle.setPosition(0.6);
-                        long startTime = System.currentTimeMillis();
-                        while (System.currentTimeMillis() - startTime < 500 && opModeIsActive()) {
-                            // Could check gamepad or update telemetry here
-                            idle();
-                        }
-                    leftArm.setPosition(0.6);
-                    rightArm.setPosition(0.45);
-                    long startTime2 = System.currentTimeMillis();
-                    while (System.currentTimeMillis() - startTime2 < 400 && opModeIsActive()) {
-                        // Could check gamepad or update telemetry here
-                        idle();
-                    }
-                    verticalClaw.setPosition(0.85);
-                    extensionClaw.setPosition(0.2);
-                    long startTime3 = System.currentTimeMillis();
-                    while (System.currentTimeMillis() - startTime3 < 400 && opModeIsActive()) {
-                        // Could check gamepad or update telemetry here
-                        idle();
-                    }
-                    leftArm.setPosition(0.2);
-                    rightArm.setPosition(0.85);
-                    long startTime4 = System.currentTimeMillis();
-                    while (System.currentTimeMillis() - startTime4 < 200 && opModeIsActive()) {
-                        // Could check gamepad or update telemetry here
-                        idle();
-                    }
-                    verticalClawAngle.setPosition(0.1);
-                    verticalPivotClaw.setPosition(0.25);
-                    transferPrep = "False";
-                    verticalGrab = "True";
-                }
+                if (isRunningSequence) {
+                    switch (step) {
+                        case 0:
+                            // Set initial positions
+                            verticalClaw.setPosition(0.65);
+                            verticalPivotClaw.setPosition(0.25);
+                            extensionPivotClaw.setPosition(0.73);
+                            extensionLeftPivot.setPosition(0);
+                            extensionRightPivot.setPosition(0.95);
+                            verticalClawAngle.setPosition(0.57);
+                            timer.reset();
+                            step++;
+                            break;
+
+                        case 1:
+                            if (timer.milliseconds() > 500) {
+                                leftArm.setPosition(0.57);
+                                rightArm.setPosition(0.47);
+                                timer.reset();
+                                step++;
+                            }
+                            break;
+
+                        case 2:
+                            if (timer.milliseconds() > 400) {
+                                verticalClaw.setPosition(0.85);
+                                extensionClaw.setPosition(0.2);
+                                timer.reset();
+                                step++;
+                            }
+                            break;
+
+                        case 3:
+                            if (timer.milliseconds() > 400) {
+                                leftArm.setPosition(0.2);
+                                rightArm.setPosition(0.85);
+                                timer.reset();
+                                step++;
+                            }
+                            break;
+
+                        case 4:
+                            if (timer.milliseconds() > 200) {
+                                verticalClawAngle.setPosition(0.1);
+                                verticalPivotClaw.setPosition(0.25);
+                                verticalGrab = "True";
+                                isRunningSequence = false;  // Done!
+                            }
+                            break;
+                    }}
 
                 if (gamepad2.left_bumper){ //Release Winch
                     leftHang.setPower(-1);
@@ -213,9 +248,8 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
                     rightHang.setPower(0);
                 }
 
-                if (gamepad1.x && wallCycling.equals( "False")){
+                if (gamepad1.x && wallCycling.equals("False")){
                     wallCycling = "True";
-                    transferPrep = "False";
                     verticalGrab = "False";
                     verticalClaw.setPosition(0.65);
                     leftArm.setPosition(0.6);
@@ -224,27 +258,29 @@ public class Covenant_Quesobowl_TeleOp extends LinearOpMode {
                 }
 
             } else {
-                leftBumperPrev = gamepad1.left_bumper;
-                rightBumperPrev = gamepad1.right_bumper;
                 //Pivot Horizontal Claw
-                if (gamepad1.left_bumper && !leftBumperPrev) {
-                    extensionPivotClaw.setPosition(Math.max(0.0, pos - 0.1));
+                if (gamepad1.left_bumper && !lastLeftBumper) {
+                    extensionPivotClaw.setPosition(Math.max(0.0, extensionPivotClaw.getPosition() - 0.2));
                 }
-                if (gamepad1.right_bumper && !rightBumperPrev) {
-                    extensionPivotClaw.setPosition(Math.min(1.0, pos + 0.1));
+
+                if (gamepad1.right_bumper && !lastRightBumper) {
+                    extensionPivotClaw.setPosition(Math.min(1.0, extensionPivotClaw.getPosition() + 0.2));
                 }
+
+                lastLeftBumper = gamepad1.left_bumper;
+                lastRightBumper = gamepad1.right_bumper;
 
                 telemetry.addData("Servo Position", extensionPivotClaw.getPosition());
                 telemetry.update(); //Telemetry for Testing
 
                 //Grab Prep & Grab Positions for Horizontal Claw
                 if (gamepad1.dpad_left) {
-                    extensionLeftPivot.setPosition(0.7);
-                    extensionRightPivot.setPosition(0.4);
+                    extensionLeftPivot.setPosition(0.65);
+                    extensionRightPivot.setPosition(0.45);
                 }
                 else if (gamepad1.dpad_right) {
-                    extensionLeftPivot.setPosition(0.8);
-                    extensionRightPivot.setPosition(0.3);
+                    extensionLeftPivot.setPosition(0.75);
+                    extensionRightPivot.setPosition(0.35);
                 }
 
                 //Horizontal Claw grab
